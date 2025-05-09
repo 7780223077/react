5ce976eb762cedd543bff13f1bc967f3662e2cd7
charan reddy <gundacharanreddy7@gmail.com>
2025-05-09 22:32:13 +0530

    Added Products list

--- src/components/fakestore/FakeStore.jsx
+++ src/components/fakestore/FakeStore.jsx
@@ -3,8 +3,9 @@ import './FakeStore.css'
 
 export function FakeStoreAPI() {
     const [categories, setCategories] = useState([])
+    const [products, setProducts] = useState([])
 
-    function loadCategories(url) {
+    function getApiService(url) {
         return fetch(url)
             .then((response) => response.json())
     }
@@ -16,40 +17,58 @@ export function FakeStoreAPI() {
             .map(word => word.charAt(0).toUpperCase() + word.slice(1))
             .join(' ');
     }
+
+    function updateProductsListToStartsWith(e) {
+        getApiService("https://fakestoreapi.com/products")
+            .then(products => {
+                const filteredProducts = products.filter(product => {
+                    return product.title.toLowerCase().startsWith(e.target.value.toLowerCase())
+                });
+                setProducts(filteredProducts);
+            })
+            .catch(error => {
+                console.error("Failed to load products:", error);
+            });
+    }
     
 
     useEffect(() => {
-        loadCategories("https://fakestoreapi.com/products/categories")
+        getApiService("https://fakestoreapi.com/products/categories")
             .then(data => {
                 setCategories(data);
             })
             .catch(error => {
                 console.error("Failed to load categories:", error);
             });
+        
+        getApiService("https://fakestoreapi.com/products")
+            .then(data => {
+                setProducts(data)
+            })
+            .catch(error => {
+                console.error("Failed to load products:", error);
+            });
     }, []);
-    
+
 
     return (
-        <div className="p-5 products">
+        <div className="ps-5 pe-5 pt-3 pb-3 products">
             <header className="d-flex justify-content-center align-items-center header rounded-4">
-                <div className="col-2 ps-5">       
+                <div className="col-1">       
                     <div className="bi bi-cart-fill cart-symbol">
                         
                     </div>
                 </div>
-                <div className="d-flex justify-content-center align-items-center col-10">
+                <div className="d-flex justify-content-center align-items-center col-11">
                     <div>
                         <div className="title">Shop the Best Deals!</div>
-                        <div><input type="text" className="form-control pt-3 pb-3 rounded-3 search-input" placeholder="Search products"/></div>
+                        <div><input type="text" className="form-control pt-2 pb-2 rounded-3 search-input" placeholder="Search products" onKeyUp={updateProductsListToStartsWith}/></div>
                     </div>
                 </div>
             </header>
             <section>
                 <div className="mt-1">
-                    <div className="pt-1 fw-bold fs-2">
-                        Categories
-                    </div>
-                    <div className="p-4 d-flex justify-content-between">
+                    <div className="pt-4 d-flex justify-content-between">
                         {
                             categories.map((category) => (
                                 <span key={category} className="fs-4 category-box">{toTitleCase(category)}</span>
@@ -58,7 +77,38 @@ export function FakeStoreAPI() {
                     </div>
                 </div>
                 <main>
-
+                    <div className="fs-2 ps-2 pt-2 fw-medium">
+                        Featured Products
+                    </div>
+                    <section>
+                        <div className="d-flex flex-wrap justify-content-between">
+                            {
+                                products.map(product => 
+                                    <div className="card m-2" key={product.id}>
+                                        <img src={product.image} className="card-img-top" />
+                                        <div className="card-body">
+                                            <h6 className="card-title">{product.title}</h6>
+                                            <div className="card-text">
+                                                {
+                                                [...Array(5)].map((_, i) => (
+                                                    <i
+                                                    key={i}
+                                                    className={`bi ${i < Math.round(product.rating?.rate) ? 'bi-star-fill' : 'bi-star'} text-warning`}
+                                                    ></i>
+                                                ))
+                                                }
+                                                <span className="ms-1 text-muted">({product.rating?.count})</span>
+                                            </div>
+                                            <p className="card-text">${product.price}</p>
+                                            <div className="text-center w-100">
+                                                <button className="btn text-dark bg-light border rounded px-4 py-2 fw-semibold">Add to Cart</button>
+                                            </div>
+                                        </div>
+                                    </div>
+                                )
+                            }
+                        </div>
+                    </section>
                 </main>
             </section>
         </div>
